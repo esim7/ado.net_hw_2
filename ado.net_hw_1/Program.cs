ef67c588c3037a2bbe2615dee64c27e48c12ad70
esim7 <makishev@yahoo.com>
2019-10-23 17:22:32 +0600

    Загрузил дз2

---
+++ ado.net_hw_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ado.net_hw_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=DESKTOP-RM1NBDJ;Database=temp;Trusted_Connection=True;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand sqlCommand = connection.CreateCommand())
+            {
+                connection.Open();
+                string query = "CREATE TABLE [Group] ( id int not null, name nvarchar(max))";
+                sqlCommand.CommandText = query;
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
